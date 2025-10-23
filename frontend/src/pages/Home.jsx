224e185def86f325e270f8104903f403ac8cd578
NikiforDragov <ndragov99@gmail.com>
2025-10-23 18:44:32 +0300

    Add searchbar to home page and useState

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import MovieCard from '../components/MovieCard';
+import { useState } from 'react';
+
 function Home() {
+    const [searchQuery, setSearchQuery] = useState('');
+
     const movies = [
         { id: 1, title: 'Harry Potter', release_date: '2001' },
         { id: 2, title: 'Fast And Furious', release_date: '2005' },
@@ -7,8 +11,27 @@ function Home() {
         { id: 4, title: 'Smile', release_date: '2022' },
     ];
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery('');
+    };
+
     return (
         <div className='home'>
+            <form onSubmit={handleSearch} className='search-form'>
+                <input
+                    type='text'
+                    placeholder='Search for movies'
+                    className='search-input'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type='submit' className='search-button'>
+                    Search
+                </button>
+            </form>
+
             <div className='movies-grid'>
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
