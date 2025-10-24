66efcde0f4953b32d9aafc8a9e9d008789961251
NikiforDragov <ndragov99@gmail.com>
2025-10-24 10:27:34 +0300

    Set up react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 import './App.css';
 
 function App() {
     return (
-        <>
-          <Home />
-        </>
+        <main className='main-content'>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/favorites' element={<Favorites />} />
+            </Routes>
+        </main>
     );
 }
 
