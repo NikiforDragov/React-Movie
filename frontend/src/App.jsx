211b88074ab39a5f3b701dcc0860acad1c5be29a
NikiforDragov <ndragov99@gmail.com>
2025-10-25 12:27:33 +0300

    Add css styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import NavBar from './components/NavBar';
-import './App.css';
+import './css/App.css';
 
 function App() {
     return (
