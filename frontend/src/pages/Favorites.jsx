dfaabd7b15d8d0eca02276e35ec55065dbb371a9
NikiforDragov <ndragov99@gmail.com>
2025-10-25 16:00:00 +0300

    Display favorite movies on favorites page

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,10 +1,29 @@
+import { useMovieContext } from '../contexts/MovieContext';
+import MovieCard from '../components/MovieCard';
 import '../css/Favorites.css';
 
 function Favorites() {
-    return <div className="favorites-empty">
-        <h2>No favorites movies yet</h2>
-        <p>Start adding movies</p>
-    </div>
+    const { favorites } = useMovieContext();
+
+    if (favorites) {
+        return (
+            <div className='favorites'>
+                <h2>Your Favorites</h2>
+                <div className='movies-grid'>
+                    {favorites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className='favorites-empty'>
+            <h2>No favorites movies yet</h2>
+            <p>Start adding movies</p>
+        </div>
+    );
 }
 
 export default Favorites;
